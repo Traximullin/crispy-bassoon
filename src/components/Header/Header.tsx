fbcc1a3af395a29361482f24749c3ace3e4f11b4
drainar09560 <drainar095601@yandex.ru>
2022-03-23 23:32:48 +0500

    В разработке: progressing-layout-header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,15 +1,26 @@
 // Functional
 import React from 'react'
+import footer from "../Footer/Footer";
 
 
 
 const Header: React.FC = () => {
     
     return (
-        <header>
-            asd
+        <header className='header'>
+            <div className='header__container container'>
+                <h1>React</h1>
+                <div className='header__address'>address</div>
+                <div className='header__search'>
+                    <input type="text" name="" id="" placeholder='asd'/>
+                    <button type="button">search</button>
+                </div>
+                <div className='header__basket'>bin</div>
+                <img className='header__avatar' src="https://static.thenounproject.com/png/363640-200.png" alt="Avatar"/>
+            </div>
         </header>
     )
 }
 
 export default Header
+

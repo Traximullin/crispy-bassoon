5377b3df2bac8ccab2dee9c335e7fc89859b14c7
drainar09560 <drainar095601@yandex.ru>
2022-03-24 22:47:41 +0500

    Разработка: layout-header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,8 +1,5 @@
 // Functional
 import React from 'react'
-import footer from "../Footer/Footer";
-
-
 
 const Header: React.FC = () => {
     
@@ -10,12 +7,18 @@ const Header: React.FC = () => {
         <header className='header'>
             <div className='header__container container'>
                 <h1>React</h1>
-                <div className='header__address'>address</div>
-                <div className='header__search'>
-                    <input type="text" name="" id="" placeholder='asd'/>
-                    <button type="button">search</button>
+                <div className='header__address'>
+                    <div>svg</div>
+                    <span>Александровск Садам Хуссейн</span>
                 </div>
-                <div className='header__basket'>bin</div>
+                <div className='header__search'>
+                    <input className='header__search_input' type="text" name="" id="" placeholder='Поиск бренда, товара, категории...'/>
+                    <button className='header__search_button' type="button">search</button>
+                </div>
+                <div className='header__basket'>
+                    <div className='header__basket_count'>10+</div>
+
+                </div>
                 <img className='header__avatar' src="https://static.thenounproject.com/png/363640-200.png" alt="Avatar"/>
             </div>
         </header>

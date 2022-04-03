01da357e5682723e18b315458f3445eadba2e120
drainar09560 <drainar095601@yandex.ru>
2022-04-03 22:42:41 +0500

    router-dom

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,6 @@
 // Functional
 import React from 'react'
+import {Link} from 'react-router-dom'
 // Assets
 import {SVG} from "../../utils/svg";
 import {Button} from "../index";
@@ -9,7 +10,9 @@ const Header: React.VFC = () => {
     return (
         <header className='header'>
             <div className='header__container container'>
-                <h1>React</h1>
+                <Link to='/'>
+                    <h1>React</h1>
+                </Link>
                 <div className='header__address'>
                     <img src={SVG.sources.location} alt="location"/>
                     <span>Александровск-Саддам-Хусейн</span>
@@ -18,10 +21,10 @@ const Header: React.VFC = () => {
                     <input className='header__search_input' type="text" name="" id="" placeholder='Поиск бренда, товара, категории...'/>
                     <Button onClick={() => {}} src={SVG.sources.search} theme='light' padding={'0px 40px'}/>
                 </div>
-                <div className='header__basket'>
+                <Link className='header__basket' to={'/basket'}>
                     <img src={SVG.sources.basket} alt="basket" className='header__basket_icon'/>
                     <div className='header__basket_count'>10+</div>
-                </div>
+                </Link>
                 <img className='header__avatar' src="https://img.freepik.com/free-vector/businessman-character-avatar-isolated_24877-60111.jpg?size=338&ext=jpg&ga=GA1.2.1615777823.1638921600" alt="Avatar"/>
             </div>
         </header>
